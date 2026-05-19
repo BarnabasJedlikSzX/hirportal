7497186e8684fb730ce13cd1349ac8bacd4c9e89
Pilter Barnabás <barnip2008@gmail.com>
2026-05-19 21:55:43 +0200

    #24 elvileg kesz

--- src/main.ts
+++ src/main.ts
@@ -13,29 +13,52 @@ const newsDiv = document.getElementById('news') as HTMLDivElement;
 const topicSort = document.getElementById('topicSort') as HTMLDivElement;
 
 let news: News[] = await GetNews();
+let sortedNews: News[] = news;
 console.log(data)
 
 function render() {
     topic();
-    newsRender();
+    newsRender(sortedNews!);
 }
 render();
 
 function topic() {
     topicSort.innerHTML = '';
-    let counter = 0;
+    topicSort.innerHTML += `<span class="m-auto"><button class='btn btn-sm btn-warning adopt-btn' data-id="Összes">Összes</button></span>`;
     topics.forEach(t => {
-        counter++;
         topicSort.innerHTML += 
         `
-            <span class="m-auto" id="${counter}">${t}</span>
+            <span class="m-auto"><button class='btn btn-sm btn-warning adopt-btn' data-id="${t}">${t}</button></span>
         `;
+    });
+    topicSort.addEventListener('click', (e) => {
+        const btn = e.target as HTMLButtonElement;
+
+        const id = btn.dataset.id;
+
+        if (!id) return;
+
+        if (id === "Összes") {
+            sortedNews = [];
+            sortedNews = news;
+        } 
+        else {
+            sortedNews = [];
+
+            news.forEach(n => {
+                if (n.topic === id) {
+                    sortedNews.push(n);
+                }
+            });
+        }
+
+        newsRender(sortedNews);
     });
 }
 
-function newsRender() {
+function newsRender(updatedNews: News[]) {
     newsDiv.innerHTML = '';
-    news.forEach(n => {
+    updatedNews.forEach(n => {
         let canEdit = false;
         let user: User = JSON.parse(data!) as User;
         if (user) {
