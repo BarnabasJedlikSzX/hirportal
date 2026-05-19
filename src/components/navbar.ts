546624f9bde6352c57f439a51bf21a52bd824f61
kizmusnoel <kizmus.noel@students.jedlik.eu>
2026-05-19 09:29:07 +0200

    navbar egyszerűsítés

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -21,10 +21,9 @@ export async function Navbar() {
         </div>
         `
 
-    if (loggedIn) {
-        const user: User = JSON.parse(loggedIn)
+    const user: User = JSON.parse(loggedIn!)
 
-        document.querySelector("#navbar")!.innerHTML += `
+    document.querySelector("#navbar")!.innerHTML += `
             <nav class="navbar bg-warning p-0">
                 <div class="container-fluid">
 
@@ -37,11 +36,18 @@ export async function Navbar() {
                 </a>
 
                 <div class="d-flex justify-content-end" style="min-width: 150px;">
-                    <div class="btn-group">
+                   ${loggedIn ?
+            `<div class="btn-group">
                     <button type="button" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         ${user.name}
                     </button>
                     <ul class="dropdown-menu dropdown-menu-end" id="mainDropdown">
+                        ${user.author ?
+                `
+                        <li style="justify-self:center;" class="w-75 m-2">
+                            <a class="btn btn-outline-light fw-bold w-100 text-center" href="edit.html" style="width:3rem;">+</a>
+                        </li>`: ""
+            }
                         <li>
                         <a class="dropdown-item" href="profile.html">Profil</a>
                         </li>
@@ -49,38 +55,18 @@ export async function Navbar() {
                         <button id="logout" class="btn text-primary dropdown-item" style="cursor: pointer;">Kijelentkezés</button>
                         </li>
                     </ul>
-                    </div>
+                    </div>`
+            :
+            `<a class="btn btn-outline-dark" href="signup_login.html">Bejelentkezés</a>`
+        }
                 </div>
 
                 </div>
             </nav>
             `
-        if (user.author) document.querySelector("#mainDropdown")!.insertAdjacentHTML("afterbegin",
-            `
-            <li style="justify-self:center;" class="w-75 m-2">
-                <a class="btn btn-outline-light fw-bold w-100 text-center" href="edit.html" style="width:3rem;">+</a>
-            </li>`
-        );
 
-    } else {
-        document.querySelector("#navbar")!.innerHTML += `
-            <nav class="navbar bg-warning p-0">
-                <div class="container-fluid">
-                    <div class="d-flex" style="min-width: 150px;">
-                        <p class="m-0">Keresés</p>
-                    </div>
 
-                    <a href="/" class="d-none d-md-block user-select-none">
-                        <img src="src/img/logo.png" style="height:5rem; margin:5px;"/>
-                    </a>
 
-                    <div class="d-flex justify-content-end" style="min-width: 150px;">
-                        <a class="btn btn-outline-dark" href="signup_login.html">Bejelentkezés</a>
-                    </div>
-                </div>
-            </nav>
-        `
-    }
     document.getElementById("logout")?.addEventListener("click", () => {
         localStorage.removeItem('aktualisUser');
         window.location.replace("/")
