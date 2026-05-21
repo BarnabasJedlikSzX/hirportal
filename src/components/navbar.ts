050d9d9747a70eb386754a62c47ac656424063fc
kizmusnoel <kizmus.noel@students.jedlik.eu>
2026-05-21 09:50:47 +0200

    #20

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -52,16 +52,16 @@ async function TopBar() {
         .slice(0, 2);
 
     document.querySelector("#navbar")!.insertAdjacentHTML("afterbegin", `
-        <div id="topBar" class="d-flex justify-content-between">
-         <p class="m-0 ms-3">
+        <div id="topBar" class="row container-fluid m-0">
+         <p class="m-0 ms-2 col">
          <span class="fw-bold">EUR:</span> ${eur.rates.HUF.toPrecision(5)} FT, <span class="fw-bold">USD:</span> ${usd.rates.HUF.toPrecision(5)} FT
          </p>
-         <p class="m-0">
+         <p class="m-0 col text-center">
          <span class="text-info">${weather.min}°</span> / 
          <span class="text-warning">${weather.max}°</span>
          <span class="fw-bold"> ${weather.description !== "" ? " - " + weather.description : ""}</span>
          </p>
-        <p class="m-0 me-3">
+        <p class="m-0 me-2 col text-end">
         <span class="fw-bold">${today.toLocaleString().slice(0, 10)} </span>
          - ${first}, ${second}
          </p>
@@ -76,37 +76,37 @@ export function Navbar() {
     TopBar()
     document.querySelector("#navbar")!.insertAdjacentHTML("beforeend", `
             <nav class="navbar bg-warning p-0">
-                <div class="container-fluid p-0 px-md-3">
+                <div class="row container-fluid p-0 px-md-3 m-0">
 
-                <div class="d-flex" style="min-width: 150px;">
+                <div class="d-flex col p-0">
                     <button class="btn btn-dark p-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><i class="bi bi-hash fs-3"></i></button>
                     ${location.pathname === "/index.html" || location.pathname === "/" ? `<button class="btn btn-light h-25 m-auto ms-3" id="search"><i class="bi bi-search me-1"></i> Keresés</button>` : ""}   
                 </div>
 
-                <a href="/" class="d-none d-md-block user-select-none">
+                <a href="/" class="d-none d-md-block user-select-none text-center col">
                     <img src="src/img/logo.png" style="height:5rem; margin:5px;"/>
                 </a>
 
-                <div class="d-flex justify-content-end" style="min-width: 150px;">
+                <div class="col text-end p-0">
                    ${loggedIn ?
-            `<div class="btn-group">
-                    <button type="button" class="btn btn-dark p-4 fw-bold dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                        ${user.name}
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end mt-3" id="mainDropdown">
-                        ${user.author ?
+            `       <div class="dropdown">
+                        <button type="button" class="btn btn-dark p-4 m-0 fw-bold dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            ${user.name}
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end mt-3" id="mainDropdown">
+                            ${user.author ?
                 `
-                        <li style="justify-self:center;" class="w-75 m-2">
-                            <a class="btn btn-outline-light fw-bold w-100 text-center" href="edit.html" style="width:3rem;">+</a>
-                        </li>`: ""
+                            <li style="justify-self:center;" class="w-75 m-2">
+                                <a class="btn btn-outline-light fw-bold w-100 text-center" href="edit.html" style="width:3rem;">+</a>
+                            </li>`: ""
             }
-                        <li>
-                        <a class="dropdown-item" href="profile.html">Profil</a>
-                        </li>
-                        <li>
-                        <button id="logout" class="btn text-primary dropdown-item" style="cursor: pointer;">Kijelentkezés</button>
-                        </li>
-                    </ul>
+                            <li>
+                            <a class="dropdown-item" href="profile.html">Profil</a>
+                            </li>
+                            <li>
+                            <button id="logout" class="btn text-primary dropdown-item" style="cursor: pointer;">Kijelentkezés</button>
+                            </li>
+                        </ul>
                     </div>`
             :
             `<a class="btn btn-dark p-4 fw-bold" href="signup_login.html">Bejelentkezés</a>`
